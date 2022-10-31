f52c093baeb117cb20a152283d633cf288b77958
hughbagan <25750667+hughbagan@users.noreply.github.com>
2022-10-30 21:30:48 -0600

    Decreased zombie path timer time (faster)

--- src/Zombie.gd
+++ src/Zombie.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-const MOVE_SPEED = 3.0
+const MOVE_SPEED = 5.0
 
 onready var raycast = $RayCast
 onready var anim_player = $AnimationPlayer
@@ -20,9 +20,11 @@ func _physics_process(delta):
 		return
 	if player == null or navigation == null:
 		return
+	
 	# Create the path to the player
 	if path_timer.is_stopped():
 		path_timer.start()
+	
 	# Walk the path
 	var direction = Vector3()
 	if path.size() > 0:
@@ -45,15 +47,6 @@ func _physics_process(delta):
 			var look_at_point = translation + direction.normalized()
 			look_at(look_at_point, Vector3.UP)
 
-#	var vec_to_player = player.translation - translation
-#	vec_to_player = vec_to_player.normalized()
-#	raycast.cast_to = vec_to_player * 1.5
-#	#move_and_collide(vec_to_player * MOVE_SPEED * delta)
-#	if raycast.is_colliding():
-#		var col = raycast.get_collider()
-#		if col != null and col.name == "Player":
-#			col.kill()
-
 func kill():
 	dead = true
 	$CollisionShape.disabled = true
@@ -67,12 +60,9 @@ func set_navigation(n):
 
 func _on_PathTimer_timeout():
 	# Calculate the path to the player
-	# TODO: Still handles corners a little weirdly.
-	#		Maybe only recalculate if player has sufficiently
-	#		strayed from the path's last point?
+	# Still handles corners slightly weird.
 	path = []
 	var p = navigation.get_simple_path(translation, player.translation, true)
 	# Fix path y translations to keep us above the floor
 	for point in p:
 		path.append(Vector3(point.x, translation.y, point.z))
-	print("New path")
